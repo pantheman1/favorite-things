b82fea7c54442681f861b95c398cd5930f4754cd
Peter Anderson <appian@Peters-MacBook-Air.local>
2025-03-11 21:46:43 -0600

    Updating the App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Favorite Things Tracker</h1>
+      <p>Start adding your favorite things!</p>
     </div>
   );
 }
-
 export default App;
