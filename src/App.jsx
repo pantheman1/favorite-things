39d740a8aef2c0a2cade59e587b5b7c2ad1305f0
Peter Anderson <peterburtonanderson@gmail.com>
2025-03-17 21:30:21 -0600

    Add FavoriteList to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import FavoriteList from "./components/FavoriteList";
+import sampleFavorites from "./data/sampleData";
 
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-600">Favorite Things Tracker</h1>
-      <p>Start adding your favorite things!</p>
+      <FavoriteList title="Movies" initialItems={sampleFavorites.movies}></FavoriteList>
+      <FavoriteList title="Books" initialItems={sampleFavorites.books}></FavoriteList>
+      <FavoriteList title="Foods" initialItems={sampleFavorites.foods}></FavoriteList>
     </div>
   );
 }
