ac367b7a703db33c2e411cbc81c01c0911c1ba9d
Peter Anderson <appian@Peters-MacBook-Air.local>
2025-03-17 21:12:54 -0600

    Add sample data

---
+++ src/data/sampleData.jsx
@@ -0,0 +1,7 @@
+const sampleFavorites = {
+    movies: ["Hook", "Truman Show", "Fall Guy", "Dune"],
+    books: ["Calvin & Hobbes", "Cradle", "Hero's Guide"],
+    foods: ["Pizza", "Caesar Salad", "Paprikas"],
+};
+
+export default sampleFavorites
